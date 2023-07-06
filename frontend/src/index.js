a07a8df1ea4a1012e4997cb1992b2f00848ba485
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2023-07-06 07:19:31 +0600

    lillte update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import Route from './route/Router';
+import ContextProvider from './contexts/ContextProvider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <ContextProvider>
     <RouterProvider router={Route}/>
-  </>
+  </ContextProvider>
 );
 
 
